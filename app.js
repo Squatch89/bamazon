2bc05ff351064eb6f3d65e6a07d2089913a49d03
Gene Williams <GeneWillb@Gmail.com>
2017-10-29 22:21:46 -0400

    connection to the databse a success

--- app.js
+++ app.js
@@ -11,8 +11,41 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    
+    showAll();
     
     connection.end();
 });
+
+function showAll() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            console.log(`ID: ${res[i].item_id} | Product: ${res[i].product_name} | Price: $${res[i].price}`);
+        }
+    });
+    
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Which product would you like to purchase? Please use the product ID",
+            name: "product"
+        },
+        {
+            type: "input",
+            message: "How many would you like to purchase?",
+            name: "ammount"
+        }
+    ]).then(function(answer) {
+    
+    });
+}
+
+
+//check to see if there is enough stock to fulfill order
+
+//if there is place the order and update the database
+//then display the total cost
+
+//else if there is not enough display (Insufficient quantity!)
+
 
